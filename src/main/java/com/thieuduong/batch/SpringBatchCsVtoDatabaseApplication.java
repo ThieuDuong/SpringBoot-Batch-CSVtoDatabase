7e7a06387e8286e330f4605d8203501e82ec0967
Thieu <duongthieu1995@gmail.com>
2020-10-04 10:07:38 +0700

    First Commit

---
+++ src/main/java/com/thieuduong/batch/SpringBatchCsVtoDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.thieuduong.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchCsVtoDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchCsVtoDatabaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thieuduong/batch/SpringBatchCsVtoDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thieuduong.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchCsVtoDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
